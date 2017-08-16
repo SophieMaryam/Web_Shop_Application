31de79868d2e133c88ab971935f0f091aaa72f5c
SophieMaryam <sophievanwersch@hotmail.com>
2017-08-16 14:38:30 +0200

    implementation of nodemailer (sending emails)

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ const app = express();
 const bodyParser = require('body-parser'); 
 app.use('/', bodyParser.urlencoded({extended:true}));
 
+const nodemailer = require('nodemailer');
+
 // Model Configuration 
 const Sequelize = require('sequelize');
 const connection = new Sequelize('', '', '', {
@@ -24,11 +26,105 @@ app.use(express.static(__dirname + "/../public"));
 // Sessions
 
 const session = require('express-session'); 
-
-
 app.use(session({
 	secret: "This is a secret",
 	resave:false, 
 	saveUninitialized: true
 
 }));
+
+
+// Contact Page 
+app.get('/contact', (req,res) => {
+	res.render('contact')
+});
+
+
+// Nodemailer
+
+app.post('/email', (req,res) => {
+	var sendingto = req.body.to,
+	sentfrom = req.body.from,
+	subject = req.body.subject
+	emailtext = req.body.text;
+
+
+	let transporter = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		port: 465,
+		secure: true,
+		service: 'Gmail',
+		auth: {
+			user: 'sophiemaryam@gmail.com',
+			pass: ''
+		},
+		tls: {
+			rejectUnauthorized:false
+		}
+	});
+
+	let mailOptions = {
+		from:sendingto, 
+		to: sentfrom,
+		subject: subject,
+		text: req.body.text,
+		html: '<b>Hello</b>'
+	};
+
+	transporter.sendMail(mailOptions, (err, info) => {
+		if (err) {
+			throw err
+		}
+		console.log('Message %s sent: %s');
+		console.log(info);
+	});
+
+}) 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// app.use('/sayHello', router);
+// router.post('/email', handleSayHello);
+// function handleSayHelo(req,res){
+// 	var transporter = nodemailer.createTransport){
+// 	server: 'Gmail',
+// 	auth: {
+// 		user: 'sophiemaryam@gmail.com',
+// 		pass: '3muscatteers'
+// 	}
+// }
+// }
+
+
+
+
+// SERVER
+
+// const server = app.listen(3000, () => {
+// 	console.log("Blog app listening on port: " + server.address().port)
+// });
