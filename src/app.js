a2a72ae54ae7c2c28a70c7b0a3ece4ef4ac01238
SophieMaryam <sophievanwersch@hotmail.com>
2017-08-21 16:23:06 +0200

    changed url names

--- src/app.js
+++ src/app.js
@@ -8,7 +8,7 @@ const nodemailer = require('nodemailer');
 
 // Model Configuration 
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('webshopapplication', process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD, {
+const sequelize = new Sequelize('', , , {
 	host: 'localhost',
 	dialect: 'postgres'
 });
@@ -25,130 +25,10 @@ app.use(express.static(__dirname + "/../public"));
 
 // Sessions
 
-const session = require('express-session'); 
-app.use(session({
-	secret: "This is a secret",
-	resave:false, 
-	saveUninitialized: true
-}));
 
 // Models
 
-const User = sequelize.define('user',{
-	firstname: {
-		type: Sequelize.STRING
-	},
-	lastname: {
-		type: Sequelize.STRING
-	},
-	username: {
-		type: Sequelize.STRING,
-		unique:true
-	},
-	email: {
-		type: Sequelize.STRING,
-		unique:true,
-		validate: {
-			isEmail:true
-		}
-	},
-	password: {
-		type: Sequelize.STRING,
-		validate: {
-			notEmpty:true
-		}
-	},
-	password_confirmation: {
-		type: Sequelize.STRING,
-		validate: {
-			notEmpty:true
-		}
-	},
-},  {
-		timestamps:false
-	});
-
-const Clothes = sequelize.define('clothes', {
-	type: Sequelize.STRING,
-	name: Sequelize.STRING,
-	color: Sequelize.STRING,
-	url: Sequelize.STRING,
-	male: Sequelize.BOOLEAN,
-	female: Sequelize.BOOLEAN,
-	price: Sequelize.INTEGER
-},	{
-		timestamps:false
-	}
-);
-
-const Wishlist = sequelize.define('wishlist', {
-	addtowishlist: Sequelize.BOOLEAN
-},	{
-		timestamps:false
-	}
-);
-
-
-User.belongsToMany(Clothes, {through: Wishlist});
-Clothes.belongsToMany(User, {through: Wishlist});
-Clothes.belongsTo(Wishlist);
-Wishlist.hasMany(Clothes);
-
 sequelize.sync({force:false});
-
-
-// Home page 
-app.get('/', (req, res) => {
-	res.render('index')
-});
-
-app.post('/searchengine', (req,res) => {
-	var input = req.body.search;
-
-	Clothes.findAll({
-		where: {
-			type:req.body.search
-		}
-	})
-	.then((selectiontype) => {
-		res.render('selection', {selectiontype:selectiontype})
-	})
-})
-
-
-// Login page
-app.get('/login', (req,res) => {
-	res.render('login')
-});
-
-// Register page
-
-app.get('/register', (req,res) => {
-	res.render('register')
-})
-
-// Profile page
-app.get('/profile', (req, res) => {
-	res.render('profile')
-});
-
-// All Clothes
-
-app.get('/collection', (req,res) => {
-	Clothes.findAll()
-	.then((eachItem) => {
-		res.render('allclothes', {eachItem: eachItem})
-	})
-	.catch((err) => {
-		console.log(err)
-	});
-});
-
-// Contact Page 
-app.get('/contact', (req,res) => {
-	res.render('contact')
-});
-
 
 // Nodemailer
 // Note: For nodemailer to work, click on this url https://www.google.com/settings/security/lesssecureapps, turn on the access to third party apps
@@ -198,20 +78,6 @@ app.post('/email', (req,res) => {
 
 }) 
 
-
-app.post('/icons', (req,res) => {
-	Wishlist.create({
-		addtowishlist:wishlist.result,
-		userID:req.session.user,
-		clothesID:wishlist.clothesid
-	})
-})
-
-// Selection
-
-app.get('/selection', (req,res) => {
-	res.render('selection')
-})
 
 // SERVER
 
