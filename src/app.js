eae057b00a9aa5abcd2a367da6506ea76b6f399a
SophieMaryam <sophievanwersch@hotmail.com>
2017-08-24 15:15:24 +0200

    wishlist for the time being removed, added css, added womens and mens search page

--- src/app.js
+++ src/app.js
@@ -7,12 +7,22 @@ app.use('/', bodyParser.urlencoded({extended:true}));
 const nodemailer = require('nodemailer');
 
 // Sessions
-
 const session = require('express-session');
+app.use(session({
+	secret: "This is a secret",
+	resave:false,
+	saveUninitialized: true,
+	store: new SequelizeStore({
+		db: sequelize,
+		checkExpirationInterval: 30 * 60 * 1000,
+		expiration : 24 * 60 * 60 * 1000
+	})
+}));
 
+// Sequelize
 const Sequelize = require('sequelize');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const sequelize = new Sequelize('web_shop_application','account1',null,  {
+const sequelize = new Sequelize('webshopapplication','postgres',1626,  {
 	host: 'localhost',
 	dialect: 'postgres',
 	storage: './session.postgres',
@@ -32,44 +42,27 @@ app.set('view engine', 'pug');
 app.use(express.static(__dirname + "/../public"));
 
 
-app.use(session({
-	secret: "This is a secret",
-	resave:false,
-	saveUninitialized: true,
-	store: new SequelizeStore({
-		db: sequelize,
-		checkExpirationInterval: 30 * 60 * 1000,
-		expiration : 24 * 60 * 60 * 1000
-	})
-}));
-
 // Model Configuration
 
-const User = sequelize.define('user',{
+const User = sequelize.define('user', {
 	firstname: {
         type: Sequelize.STRING
     },
     lastname: {
         type: Sequelize.STRING
     },
-    username: {
-        type: Sequelize.STRING,
-        unique:true
-    },
     email: {
         type: Sequelize.STRING,
         unique:true,
     },
     password: {
-        type: Sequelize.STRING,
+        type: Sequelize.STRING
     },
     password_confirmation: {
-        type: Sequelize.STRING,
-        validate: {
-            notEmpty:true
-        }
-    }
-    }); 
+		type: Sequelize.STRING
+	}
+}); 
+
 const Clothes = sequelize.define('clothes',{
 	type: Sequelize.STRING,
 	name: Sequelize.STRING,
@@ -80,127 +73,34 @@ const Clothes = sequelize.define('clothes',{
 	price: Sequelize.INTEGER
 });
 
-const Wishlist = sequelize.define('wishlist',{
-	addToWishList: Sequelize.BOOLEAN
-});
+// const Wishlist = sequelize.define('wishlist',{
+// 	id: {
+// 		type: Sequelize.INTEGER,
+//         primaryKey: true 
+//     },
+// 	addToWishList: Sequelize.BOOLEAN
+// });
 
 
 // relationships
 
-User.belongsToMany(Clothes, {through: Wishlist});
-Clothes.belongsToMany(User, {through: Wishlist});
-Clothes.belongsTo(Wishlist);
-Wishlist.hasMany(Clothes)
+// User.hasMany(Clothes);
+// Clothes.belongsTo(User);
 
-sequelize.sync({force:false});
+// Clothes.hasMany(User);
+
+// Wishlist.hasMany(Clothes);
+// Wishlist.belongsTo(User);
+
+// sequelize.sync({force:true});
 
 
-//rendering register page
+// Home page
 app.get('/',(req,res) => {
-	res.render('register')
+	var user = req.session.user;
+	res.render('index', {user:user})
 });
 
-// registering and storing new user in the database
-app.post('/register',(req,res) => {
-
-	User.create({
-		firstName: req.body.inputFirstName,
-		lastName: req.body.inputLastName,
-		email: req.body.inputEmail,
-		password: req.body.inputPassword
-	}).then((user) => {
-			req.session.user = user;
-			res.redirect('/profile');
-	});
-});
-
-app.get('/login', (req,res) => {
-	res.render('login')
-});
-
-// profile PAGE
-
-app.get('/profile',(req,res) => {
-  var user = req.session.user
-  res.render('profile',{user: user})
-});
-
-//creating login
-
-app.post('/login', bodyParser.urlencoded({extended:true}),(req,res) => {
-
-  User.findOne({
-    where: {
-      email: req.body.inputEmail
-    }
-  }).then ((user) => {
-    if (user !== undefined && req.body.inputPassword == user.password) {
-      req.session.user = user;
-      res.redirect('/profile');
-    } else{
-        res.redirect('/login?message=' + encodeURIComponent('Invalid email or password'));
-      }
-
-  })
-  .catch( error => {
-      res.redirect('/login?message=' + encodeURIComponent('Invalid email or password'));
-    });
-});
-
-// log out
-
-app.get('/logout',(req,res) =>{
-  req.session.destroy((err) =>{
-    if (err){
-      throw (err);
-    }
-    res.redirect('/?login=' + encodeURIComponent('Logged out successfully'));
-  });
-});
-
-// Collection page (All clothes)
-app.get('/collection',(req,res) => {
-
-	Clothes.findAll()
-	.then((eachItem) => {
-		res.render('allclothes',{eachItem: eachItem})
-	})
-	.catch((err) => {
-		console.log(err)
-	})
-});
-
-// Search page 
-app.get('/search',(req,res) =>{
-	res.render('search')
-});
-
-app.post('/search', (req,res) => {
-    var input = req.body.search;
-
-		Clothes.findOne({
-        where: {
-            type: input
-        }
-    })
-    .then((clothestype) => {
-			res.render('shoes',{clothestype: clothestype});
-			
-	});
-});
-
-// Collection page - wishlist icon
-app.post('/icons', (req,res) => {
-	console.log(req.body);
-
-    Wishlist.create({
-        addToWishList: req.body.result,
-        userId:req.session.user.id,
-        clotheId: req.body.clothesid
-    })
-});
-
-// Home page search engine
 app.post('/searchengine', (req,res) => {
     var input = req.body.search;
 
@@ -212,16 +112,243 @@ app.post('/searchengine', (req,res) => {
     .then((selectiontype) => {
         res.render('selection', {selectiontype:selectiontype})
     })
-})
+});
+
+
+// All Womens Clothes
+app.get('/women', (req,res) => {
+	var user = req.session.user;
+	var boolean = true;
+	Clothes.findAll({
+		where: {
+			female:boolean
+		}
+	})
+	.then((female) => {
+		res.render('womenclothes', {female:female, user:user})
+	})
+});
+
+// All mens clothes
+app.get('/men', (req,res) => {
+	var user = req.session.user;
+	var boolean = true;
+	Clothes.findAll({
+		where: {
+			male:boolean
+		}
+	})
+	.then((male) => {
+		res.render('menclothes', {male:male, user:user})
+	})
+});
+
+
+
+// Register
+app.get('/register',(req,res) => {
+	var user = req.session.user;
+	res.render('register', {user:user})
+});
+
+app.post('/register', (req,res) => {
+	let password = req.body.inputPassword,
+	email = req.body.inputEmail,
+	firstname = req.body.inputFirstName,
+	lastname = req.body.inputLastName,
+	pwconfirmation = req.body.passwordconfirmation;
+	console.log('pwconfirmation ' + pwconfirmation);
+	console.log('password: ' + password);
+	
+	if(password !== pwconfirmation){
+		throw new Error("Password confirmation doesn't match.")
+	} else if (password === pwconfirmation) {
+		bcrypt.hash(password, 10, (err, hash) => {
+			console.log("the hash" + hash)
+			if(err){
+				throw err;
+			}
+
+		User.create({
+			firstname:firstname,
+			lastname:lastname,
+			email: email,
+			password: hash,
+			password_confirmation: hash
+		})
+		.then((user) => {
+			req.session.user = user;
+			res.redirect(`/profile`);
+		})
+		.catch((err) => {
+			console.log("Error" + err);
+		})
+	
+	})
+	}
+});
+
+// Login 
+app.get('/login', (req,res) => {
+	var user = req.session.user;
+	if(!user){
+		res.render('login')
+	} else {
+		res.redirect(`/profile`)
+	}
+});
+
+
+app.post('/login', bodyParser.urlencoded({extended:true}),(req,res) => {
+
+	User.findOne({
+	where: {
+	  email: req.body.inputEmail
+	}
+	})
+	.then ((user) => {
+	console.log('does it reach this??' + user)
+	   if(!user){
+				res.redirect('/?message=' + encodeURIComponent("Invalid email or password"));
+		} else {
+			console.log('nnumber 2')
+			bcrypt.compare(req.body.inputPassword, user.password, (err, result) => { // first argument is the password the user typed in, and thes second is the one in the database
+				if(err){
+					console.log(err)
+				} else {
+					if(result === true){
+						req.session.user = user;
+						res.redirect(`/profile`)
+					} else {
+						console.log("Error")
+					}
+				}
+			})
+		}
+	})
+	.catch(function(err){
+		console.log("Error" + err)
+		res.redirect('/?message=' + encodeURIComponent("Invalid email or password."));
+	});
+}); 
+
+	// log out
+
+app.get('/logout',(req,res) => {
+  req.session.destroy((err) => {
+    if (err){
+      throw (err);
+    }
+    res.redirect('/?login=' + encodeURIComponent('Logged out successfully'));
+  });
+});
+
+
+
+
+// Profile
+
+app.get('/profile',(req,res) => {
+  var user = req.session.user;
+
+  if(!user){
+		res.redirect('/?message=' + encodeURIComponent("Please log in."));
+	} else {
+		res.render('profile', {user: user});
+	// 	Wishlist.findAll({
+	// 		where: {
+	// 			userId:user.id
+	// 		}
+	// 	})
+	// 	.then((one) => {
+	// 		console.log("ayo" + one);
+	// 		console.log("ay" + one.clotheId);
+	// 		Clothes.findAll({
+	// 			where: {
+	// 				id:one
+	// 			}
+	// 		})
+	// 		.then((wishlist) => {
+	// 			res.render('profile', {user: user, wishlist:wishlist})
+	// 		})
+	// 	})
+	// 	.catch((err) => {
+	// 		console.log('Error: ' + err);
+	// })
+	}
+});
+
+app.post('/profilesearchengine', (req,res) => {
+    var input = req.body.profilesearchengine;
+
+    Clothes.findAll({
+        where: {
+            type:input
+        }
+    })
+    .then((selectiontype) => {
+        res.render('selection', {selectiontype:selectiontype})
+    })
+    .catch((err) => {
+			console.log('Error: ' + err);
+	})
+});
+
+
+// Collection page (All clothes)
+app.get('/collection',(req,res) => {
+	var user = req.session.user;
+	Clothes.findAll()
+	.then((eachItem) => {
+		res.render('allclothes',{eachItem: eachItem, user:user})
+	})
+	.catch((err) => {
+		console.log(err)
+	})
+});
+
+// app.post('/icons', (req,res) => {
+// 	console.log(req.body.clothesid);
+// 	const user = req.session.user;
+
+//     Wishlist.create({
+//         addToWishList: req.body.result,
+//         userId:user.id,
+//         clotheId: req.body.clothesid
+//    })
+// });
+
+// Search page 
+app.get('/search',(req,res) =>{
+	res.render('search')
+});
+
+app.post('/search', (req,res) => {
+    var input = req.body.search;
+
+		Clothes.findAll({
+        where: {
+            type: input
+        }
+    })
+    .then((selectiontype) => {
+			res.render('selection', {selectiontype: selectiontype});
+			
+	});
+});
+
 
 // Selection Page
 app.get('/selection', (req,res) => {
-    res.render('selection')
+   var user = req.session.user;
+    res.render('selection', {user:user})
 })
+
 
 // Contact page
 app.get('/contact', (req,res) => {
-    res.render('contact')
+   	var user = req.session.user;
+    res.render('contact', {user:user})
 });
 
   // Nodemailer
@@ -269,10 +396,9 @@ app.post('/email', (req,res) => {
 		console.log('Message %s sent: %s');
 		console.log('info' + info);
 	})
-
 }) 
 
 // Server
 app.listen(3001, () => {
-  console.log('App is working on port 3000');
+  console.log('App is working on port 3001');
 });
